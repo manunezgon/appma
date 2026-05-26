dbafe749ab474b288a4bbd070736431883744b54
Ana <anapaulaquinoa@gmail.com>
2026-05-26 10:59:30 +0200

    [MODIFY] Relocate news tab from TabLayout component

--- frontend/app/(tabs)/_layout.jsx
+++ frontend/app/(tabs)/_layout.jsx
@@ -23,6 +23,15 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
+        name="news"
+        options={{
+          title: "Noticias",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="newspaper" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="index"
         options={{
           title: "Home",
@@ -46,15 +55,6 @@ export default function TabLayout() {
           title: "Ranking",
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="podium" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="news"
-        options={{
-          title: "Noticias",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="newspaper" size={size} color={color} />
           ),
         }}
       />
