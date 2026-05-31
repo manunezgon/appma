5a4bcd3e9fae36cc49b2c81dcd841c08a33ac4a9
Ana <anapaulaquinoa@gmail.com>
2026-05-31 12:05:08 +0200

    [MODIFY] Update API_BASE_URL to use production URL and comment out local host configuration

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -2,4 +2,7 @@ import Constants from "expo-constants";
 
 const host = Constants.expoConfig?.hostUri?.split(":")[0] || "localhost";
 
-export const API_BASE_URL = `http://${host}:8080`;
+//export const API_BASE_URL = `http://${host}:8080`;
+
+// config.js o .env
+export const API_BASE_URL = 'https://appma-production-0a76.up.railway.app';
